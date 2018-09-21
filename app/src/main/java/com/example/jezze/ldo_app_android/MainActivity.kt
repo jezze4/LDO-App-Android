47e2bec063dc9fc5539ee254b673a9210a460ddb
Jezze Martinez <jezze.04@gmail.com>
2018-09-21 15:03:36 -0500

    Buttons change color when pressed. started adding functionality to launch into a different activty when button is pressed

--- app/src/main/java/com/example/jezze/ldo_app_android/MainActivity.kt
+++ app/src/main/java/com/example/jezze/ldo_app_android/MainActivity.kt
@@ -2,11 +2,27 @@ package com.example.jezze.ldo_app_android
 
 import android.app.Activity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //add addjob button
+        val button_addjob = findViewById<Button>(R.id.add_button)
+
+        button_addjob.setOnClickListener {}
+
+        fun launch_addListing(view: View){
+
+        }
+        fun setOnClickListener() {
+            fun onClick(view: View){
+
+            }
+        }
     }
 }
